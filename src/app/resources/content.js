2c4e53bd64b4e469f84eb0bbf885f4b723685922
Shreyas Pasumarthi <47158029+ShreyasPasumarthi@users.noreply.github.com>
2024-12-10 02:48:56 -0800

    Change name

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,8 +1,8 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
+    firstName: 'Shreyas',
+    lastName:  'Pasumarthi',
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
