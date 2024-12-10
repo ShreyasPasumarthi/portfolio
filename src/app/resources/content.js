4bf390e1fe7c67a3b1ad2cead87fb8e5481c44ca
Shreyas Pasumarthi <47158029+ShreyasPasumarthi@users.noreply.github.com>
2024-12-10 03:05:14 -0800

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
     },
     role:      'Design Engineer',
     avatar:    '/images/avatar.jpg',
-    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    location:  'US/Pacific',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English']  // optional: Leave the array empty if you don't want to display languages
 }
 
